31002d73d7b5cda0891b42706b6924a96fdb4177
Yaku Brang Ja <dbrangja@gmail.com>
2021-12-06 16:23:17 +0630

    reworked the responsive menu window animation

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const menuBtn = document.querySelector(".menu-btn");
+const menuBtn2 = document.querySelector(".menu-btn2");
 const rqBtn = document.querySelector(".request-btn");
 const navigation = document.querySelector(".navigation");
 const closeWindow = document.querySelector(".close-window");
@@ -8,14 +9,22 @@ menuBtn.onclick = () => {
   navigation.classList.toggle("navigation-active");
   navigation.style.display = "flex";
   menuBtn.classList.toggle("menu-btn-active");
+  menuBtn2.classList.add("menu-btn2-active");
   closeWindow.style.display = "block";
+};
+function closeMenu() {
+  navigation.classList.remove("navigation-active");
+  menuBtn.classList.remove("menu-btn-active");
+  menuBtn2.classList.remove("menu-btn2-active");
+  formWindow.classList.remove("section-login-active");
+  closeWindow.style.display = "none";
+}
+menuBtn2.onclick = () => {
+  closeMenu();
 };
 
 closeWindow.onclick = () => {
-  navigation.classList.remove("navigation-active");
-  menuBtn.classList.remove("menu-btn-active");
-  formWindow.classList.remove("section-login-active");
-  closeWindow.style.display = "none";
+  closeMenu();
 };
 
 rqBtn.onclick = () => {
